2d1abd1d2d9055abf6300dc50fa2eff558493af0
unknown <luster.clifford@yahoo.com>
2022-10-31 19:48:00 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import { useForm } from 'react-hook-form';
 
-function App() {
+export default function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="main-form">
+      <input {...register("firstName", { required: true, maxLength: 20 })} placeholder="John" />
+      <input {...register("lastName", { required: true, maxLength: 20, pattern: /^[A-Za-z]+$/i })}
+      placeholder="Smith" className='form-input' />
+      <select {...register("country")} className="form-input">
+        <option value="USA">USA</option>
+        <option value="italy">Italy</option>
+        <option value="france">France</option>
+      </select>
+      <input type="submit" className='form-input' />
+    </form>
   );
 }
-
-export default App;
